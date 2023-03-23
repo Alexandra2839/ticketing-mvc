34545013980405a5d73e0c173a988d535986b031
Sasha <alexandra.aslobodian@gmail.com>
2023-03-23 18:57:47 -0400

    created CrudService interface and updated User and Role svs

--- src/main/java/com/learn/service/RoleService.java
+++ src/main/java/com/learn/service/RoleService.java
@@ -1,16 +1,9 @@
 package com.learn.service;
 
 import com.learn.dto.RoleDTO;
-import com.learn.dto.UserDTO;
 
-import java.util.List;
 
-public interface RoleService {
-
-    RoleDTO save(RoleDTO role);
-    RoleDTO findById(Long id);
-    List<RoleDTO> findAll();
-    void deleteById(Long id);
+public interface RoleService extends CrudService<RoleDTO, Long>{
 
 
 
--- src/main/java/com/learn/service/UserService.java
+++ src/main/java/com/learn/service/UserService.java
@@ -1,14 +1,9 @@
 package com.learn.service;
 
-import com.learn.dto.RoleDTO;
 import com.learn.dto.UserDTO;
 
-import java.util.List;
 
-public interface UserService {
+public interface UserService extends CrudService<UserDTO, String>{
 
-    UserDTO save(UserDTO role);
-    UserDTO findById(String username);
-    List<UserDTO> findAll();
-    void deleteById(String username);
+
 }
