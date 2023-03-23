af9a487f62ebe61977fbb9f6f337c5a0a820a165
Sasha <alexandra.aslobodian@gmail.com>
2023-03-23 18:58:00 -0400

    created CrudService interface

---
+++ src/main/java/com/learn/service/CrudService.java
@@ -0,0 +1,13 @@
+package com.learn.service;
+
+import com.learn.dto.UserDTO;
+
+import java.util.List;
+
+public interface CrudService <T, ID>{
+
+    T save(T role);
+    T findById(ID username);
+    List<T> findAll();
+    void deleteById(ID username);
+}
