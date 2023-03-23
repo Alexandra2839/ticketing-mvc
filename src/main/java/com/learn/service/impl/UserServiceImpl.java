caac3762d63df81a67583d98621fe02e9b389d36
Sasha <alexandra.aslobodian@gmail.com>
2023-03-23 19:31:11 -0400

    created UserService Impl

---
+++ src/main/java/com/learn/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.learn.service.impl;
+
+import com.learn.dto.UserDTO;
+import com.learn.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
+    @Override
+    public UserDTO save(UserDTO user) {
+        return super.save(user.getUsername(), user);
+    }
+
+    @Override
+    public UserDTO findById(String username) {
+        return super.findById(username);
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(String username) {
+        super.deleteById(username);
+    }
+}
