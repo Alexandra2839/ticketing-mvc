3d08712c108b39db7f3aa5153b9beeadcd0ee757
Sasha <alexandra.aslobodian@gmail.com>
2023-03-24 19:55:23 -0400

    added @DataTimeFormat to fields

--- src/main/java/com/learn/dto/ProjectDTO.java
+++ src/main/java/com/learn/dto/ProjectDTO.java
@@ -4,7 +4,7 @@ import com.learn.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.springframework.format.annotation.DateTimeFormat;
 
 
 import java.time.LocalDate;
@@ -16,9 +16,10 @@ public class ProjectDTO {
 
     private String projectName;
     private String projectCode;
-
     private UserDTO assignedManager;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     private String projectDetail;
     private Status projectStatus;
