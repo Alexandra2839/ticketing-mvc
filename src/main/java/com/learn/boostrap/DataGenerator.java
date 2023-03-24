427a372164a246cd85f1e7a230da8f067db10210
Sasha <alexandra.aslobodian@gmail.com>
2023-03-23 20:39:59 -0400

    add users to DB

--- src/main/java/com/learn/boostrap/DataGenerator.java
+++ src/main/java/com/learn/boostrap/DataGenerator.java
@@ -1,6 +1,8 @@
 package com.learn.boostrap;
 
 import com.learn.dto.RoleDTO;
+import com.learn.dto.UserDTO;
+import com.learn.enums.Gender;
 import com.learn.service.impl.RoleServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -22,6 +24,26 @@ public class DataGenerator implements CommandLineRunner {
         RoleDTO employeeRole = new RoleDTO(3L, "Employee");
 
         roleService.save(adminRole);
+        roleService.save(managerRole);
+        roleService.save(employeeRole);
+
+        UserDTO user1 = new UserDTO("John", "Kesy",
+                "john@cydeo.com", "Abc1", true, "7459684532", managerRole, Gender.MALE);
+        UserDTO user5 = new UserDTO("Mike", "Smith",
+                "mike@cydeo.com", "Abc2", true, "7459684532", adminRole, Gender.MALE);
+        UserDTO user2 = new UserDTO("Delisa",
+                "Norre", "delisa@cydeo.com", "123", true, "8567412358", managerRole, Gender.FEMALE);
+        UserDTO user3 = new UserDTO("Craig", "Jark",
+                "craig@cydeo.com", "Abc3", true, "7777775566", employeeRole, Gender.MALE);
+        UserDTO user4 = new UserDTO("Shaun",
+                "Hayns", "shaun@cydeo.com", "Abc4", true, "3256987412", managerRole, Gender.MALE);
+        UserDTO user6 = new UserDTO("Elizebeth",
+                "Loren", "elizebeth@cydeo.com", "Abc4", true, "5306987412", employeeRole, Gender.FEMALE);
+        UserDTO user7 = new UserDTO("Maria",
+                "Ada", "maria@cydeo.com", "Abc4", true, "9996987412", employeeRole, Gender.FEMALE);
+        UserDTO user8 = new UserDTO("Bill",
+                "Matt", "bill@cydeo.com", "Abc4", true, "8881239846", employeeRole, Gender.MALE);
+
 
 
     }
