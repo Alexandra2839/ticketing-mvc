6301af53808161494cd8a817aa40d369b28e9a1d
Sasha <alexandra.aslobodian@gmail.com>
2023-03-25 19:21:54 -0400

    added getCountedListOfProjects method

--- src/main/java/com/learn/service/ProjectService.java
+++ src/main/java/com/learn/service/ProjectService.java
@@ -1,9 +1,14 @@
 package com.learn.service;
 
 import com.learn.dto.ProjectDTO;
+import com.learn.dto.UserDTO;
+
+import java.util.List;
 
 public interface ProjectService extends CrudService<ProjectDTO, String> {
 
     void complete(ProjectDTO project);
+
+    List<ProjectDTO> getCountedListOfProject(UserDTO manager);
 
 }
