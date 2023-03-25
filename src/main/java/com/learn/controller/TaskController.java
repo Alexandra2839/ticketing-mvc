4d6bf8ed22f6aaee299f5a194d4af305526a4d94
Sasha <alexandra.aslobodian@gmail.com>
2023-03-24 22:54:38 -0400

    added attributes to controller

--- src/main/java/com/learn/controller/TaskController.java
+++ src/main/java/com/learn/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.learn.controller;
 
 import com.learn.dto.TaskDTO;
+import com.learn.service.ProjectService;
+import com.learn.service.TaskService;
+import com.learn.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/task")
 public class TaskController {
 
+    private final ProjectService projectService;
+    private final UserService userService;
+    private final TaskService taskService;
+
+
+    public TaskController(ProjectService projectService, UserService userService, TaskService taskService) {
+        this.projectService = projectService;
+        this.userService = userService;
+        this.taskService = taskService;
+    }
+
     @GetMapping("/create")
     public String taskCreate(Model model){
         model.addAttribute("task", new TaskDTO());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees() );
+
+        model.addAttribute("tasks", taskService.findAll());
 
 
         return "/task/create";
