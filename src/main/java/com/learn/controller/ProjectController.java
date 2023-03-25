970de49cd27000015c9b98fc1270ff0235338e99
Sasha <alexandra.aslobodian@gmail.com>
2023-03-25 19:23:23 -0400

    added getProjectByManager controller

--- src/main/java/com/learn/controller/ProjectController.java
+++ src/main/java/com/learn/controller/ProjectController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
@@ -78,6 +80,18 @@ public class ProjectController {
 
 
         return "redirect:/project/create";
+    }
+
+    @GetMapping("/manager/project-status")
+    public String getProjectByManager(Model model){
+
+        UserDTO manager = userService.findById("john@cydeo.com");
+
+        List<ProjectDTO> projects = projectService.getCountedListOfProject(manager);
+
+        model.addAttribute("projects", projects);
+
+        return "/manager/project-status";
     }
 
 }
