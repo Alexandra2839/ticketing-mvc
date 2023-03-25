a866cffbc1c2f9099fab0d20531c620165138160
Sasha <alexandra.aslobodian@gmail.com>
2023-03-24 22:41:16 -0400

    added complete method

--- src/main/java/com/learn/service/ProjectService.java
+++ src/main/java/com/learn/service/ProjectService.java
@@ -3,4 +3,7 @@ package com.learn.service;
 import com.learn.dto.ProjectDTO;
 
 public interface ProjectService extends CrudService<ProjectDTO, String> {
+
+    void complete(ProjectDTO project);
+
 }
