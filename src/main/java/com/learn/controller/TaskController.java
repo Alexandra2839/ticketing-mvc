fe083e80b2bf5f032e9ea4f72753f595dc17bfad
Sasha <alexandra.aslobodian@gmail.com>
2023-03-24 22:44:31 -0400

    added task controller for /create

---
+++ src/main/java/com/learn/controller/TaskController.java
@@ -0,0 +1,20 @@
+package com.learn.controller;
+
+import com.learn.dto.TaskDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+
+    @GetMapping("/create")
+    public String taskCreate(Model model){
+        model.addAttribute("task", new TaskDTO());
+
+
+        return "/task/create";
+    }
+}
