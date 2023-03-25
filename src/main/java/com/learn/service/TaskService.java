74183106fc9e8f86da30716a9fdfe773c7f0b509
Sasha <alexandra.aslobodian@gmail.com>
2023-03-25 19:22:34 -0400

    added find tasks by manager method

--- src/main/java/com/learn/service/TaskService.java
+++ src/main/java/com/learn/service/TaskService.java
@@ -1,6 +1,11 @@
 package com.learn.service;
 
 import com.learn.dto.TaskDTO;
+import com.learn.dto.UserDTO;
+
+import java.util.List;
 
 public interface TaskService extends CrudService<TaskDTO, Long>{
+
+    List<TaskDTO> findTasksByManager(UserDTO manager);
 }
