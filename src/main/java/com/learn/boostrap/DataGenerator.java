4e883a17ce90a8fb2ac6222f4ba0068f792b5970
Sasha <alexandra.aslobodian@gmail.com>
2023-03-24 22:43:02 -0400

    added tasks to custom db

--- src/main/java/com/learn/boostrap/DataGenerator.java
+++ src/main/java/com/learn/boostrap/DataGenerator.java
@@ -2,12 +2,14 @@ package com.learn.boostrap;
 
 import com.learn.dto.ProjectDTO;
 import com.learn.dto.RoleDTO;
+import com.learn.dto.TaskDTO;
 import com.learn.dto.UserDTO;
 import com.learn.enums.Gender;
 import com.learn.enums.Status;
 import com.learn.service.ProjectService;
+import com.learn.service.RoleService;
+import com.learn.service.TaskService;
 import com.learn.service.UserService;
-import com.learn.service.impl.RoleServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,14 +18,16 @@ import java.time.LocalDate;
 @Component
 public class DataGenerator implements CommandLineRunner {
 
-    private final RoleServiceImpl roleService;
+    private final RoleService roleService;
     private final UserService userService;
     private final ProjectService projectService;
+    private final TaskService taskService;
 
-    public DataGenerator(RoleServiceImpl roleService, UserService userService, ProjectService projectService) {
+    public DataGenerator(RoleService roleService, UserService userService, ProjectService projectService, TaskService taskService) {
         this.roleService = roleService;
         this.userService = userService;
         this.projectService = projectService;
+        this.taskService = taskService;
     }
 
     @Override
@@ -70,6 +74,16 @@ public class DataGenerator implements CommandLineRunner {
         projectService.save(project1);
         projectService.save(project2);
         projectService.save(project3);
+
+        TaskDTO task1 = new TaskDTO(1L,project1, user8, "Controller", "Request Mapping", Status.IN_PROGRESS, LocalDate.now().minusDays(4));
+        TaskDTO task2 = new TaskDTO(2L,project3, user3, "Configuration", "Database Connection", Status.COMPLETED, LocalDate.now().minusDays(12));
+        TaskDTO task3 = new TaskDTO(3L,project3, user6, "Mapping", "One-To-Many", Status.COMPLETED, LocalDate.now().minusDays(8));
+        TaskDTO task4 = new TaskDTO(4L,project2, user7, "Dependency Injection", "Autowired", Status.IN_PROGRESS, LocalDate.now().minusDays(20));
+
+        taskService.save(task1);
+        taskService.save(task2);
+        taskService.save(task3);
+        taskService.save(task4);
 
     }
 }
