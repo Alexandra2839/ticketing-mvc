1b77e90c58247a9f28235af421043e95bdf5f1bc
Sasha <alexandra.aslobodian@gmail.com>
2023-04-11 17:04:39 -0400

    updated last commit

--- src/main/java/com/learn/TicketingProjectMvcApplication.java
+++ src/main/java/com/learn/TicketingProjectMvcApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketingProjectMvcApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectMvcApplication.class, args);
     }
